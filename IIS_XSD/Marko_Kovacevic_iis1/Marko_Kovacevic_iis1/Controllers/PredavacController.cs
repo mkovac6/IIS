b9539a9637df467e9744afc228eb50bd12199fe2
Marko Kovačević <mkovac6@racunarstvo.hr>
2023-01-10 13:46:19 +0100

    REST API sučelje koje uključuje servis koji će se pozivati POST metodom i slati XML datoteku za spremanje novog entiteta u sustavu koja će se validirati korištenjem XSD datoteke

---
+++ IIS_XSD/Marko_Kovacevic_iis1/Marko_Kovacevic_iis1/Controllers/PredavacController.cs
@@ -0,0 +1,96 @@
+﻿using Marko_Kovacevic_iis1.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Marko_Kovacevic_iis1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredavacController : ControllerBase
+    {
+        /*[HttpPost("validate-xsd")]
+        public bool ValidateXsd(XmlElement predavacArray)
+        {
+            try 
+            {
+                bool f = false;
+                XmlDocument documentXML = predavacArray.OwnerDocument;
+                documentXML.AppendChild(predavacArray);
+
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add("http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model", Path.GetFullPath("predavac_schema.xsd"));
+                XmlReader xmlReader = new XmlNodeReader(documentXML);
+                XDocument document = XDocument.Load(xmlReader);
+
+                document.Validate(schemaSet, (o, e) =>
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    f = true;
+                });
+
+                if (!f)
+                {
+                    DataContractSerializer deserialization = new DataContractSerializer(typeof(PredavacArray));
+                    MemoryStream memoryStream = new MemoryStream();
+                    document.Save(memoryStream);
+                    memoryStream.Position = 0;
+
+                    PredavacArray pred = (PredavacArray)deserialization.ReadObject(memoryStream);
+
+                    foreach (var item in pred.PredavacList)
+                    {
+                        Startup.PredavacArray.PredavacList.Add(item);
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch
+            {
+
+                return false;
+            }
+        }*/
+
+        private List<PredavacArray> ListPredavac;
+
+        public PredavacController(List<PredavacArray> listPredavac)
+        {
+            ListPredavac = listPredavac;
+        }
+
+        public List<PredavacArray> Get()
+        {
+            return ListPredavac;
+        }
+
+
+        [HttpPost]
+        public void Post(PredavacArray newPredavac)
+        {
+            if (newPredavac != null)
+            {
+                ListPredavac.Add(newPredavac);
+                HttpContext.Response.StatusCode = StatusCodes.Status201Created;
+            }
+            else
+            {
+                HttpContext.Response.StatusCode = StatusCodes.Status400BadRequest;
+            }
+        }
+    }
+}
---
+++ IIS_XSD/Marko_Kovacevic_iis1/Marko_Kovacevic_iis1/Startup.cs
@@ -0,0 +1,79 @@
+﻿using Marko_Kovacevic_iis1.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Marko_Kovacevic_iis1
+{
+    internal class Startup
+    {
+        //internal static PredavacArray PredavacArray;
+        private List<PredavacArray> listPredavac;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            listPredavac = new List<PredavacArray>
+            {
+                new PredavacArray
+                {
+                    Id = "1",
+                    Type = "Visi predavac",
+                    Name = "Marko",
+                    Placa = 8000
+                },
+                new PredavacArray
+                {
+                    Id = "1",
+                    Type = "Voditelj studija",
+                    Name = "Ana",
+                    Placa = 10000
+                },
+                new PredavacArray
+                {
+                    Id = "1",
+                    Type = "Predavac",
+                    Name = "Maja",
+                    Placa = 7000
+                }
+            };
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers().AddXmlDataContractSerializerFormatters();
+
+            services.AddSingleton<List<PredavacArray>>(listPredavac);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
