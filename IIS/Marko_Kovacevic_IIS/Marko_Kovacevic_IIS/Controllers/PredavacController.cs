daebc42ba5b7f404340b1078259cf6818144c7bc
Marko Kovačević <mkovac6@racunarstvo.hr>
2023-01-11 13:00:59 +0100

    REST API sučelje koje uključuje servis koji će se pozivati POST metodom i slati XML datoteku za spremanje novog entiteta u sustavu koja će se validirati korištenjem XSD datoteke (take 2)

---
+++ IIS/Marko_Kovacevic_IIS/Marko_Kovacevic_IIS/Controllers/PredavacController.cs
@@ -0,0 +1,69 @@
+﻿using Marko_Kovacevic_iis.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+using System.Xml.Schema;
+
+namespace Marko_Kovacevic_iis.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PredavacController : ControllerBase
+    {
+        [HttpPost("validate-xsd")]
+        public bool ValidateXsd(XmlElement predavacArray)
+        {
+            try
+            {
+                bool f = false;
+                XmlDocument documentXML = predavacArray.OwnerDocument;
+                documentXML.AppendChild(predavacArray);
+
+                XmlSchemaSet schemaSet = new XmlSchemaSet();
+                schemaSet.Add("http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model", Path.GetFullPath("predavac_schema.xsd"));
+                XmlReader xmlReader = new XmlNodeReader(documentXML);
+                XDocument document = XDocument.Load(xmlReader);
+
+                document.Validate(schemaSet, (o, e) =>
+                {
+                    Console.WriteLine("{0}", e.Message);
+                    f = true;
+                });
+
+                if (!f)
+                {
+                    DataContractSerializer deserialization = new DataContractSerializer(typeof(PredavacArray));
+                    MemoryStream memoryStream = new MemoryStream();
+                    document.Save(memoryStream);
+                    memoryStream.Position = 0;
+
+                    PredavacArray pred = (PredavacArray)deserialization.ReadObject(memoryStream);
+
+                    foreach (var item in pred.PredavacList)
+                    {
+                        Startup.PredavacArray.PredavacList.Add(item);
+                    }
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            catch
+            {
+
+                return false;
+            }
+        }
+    }
+}
---
+++ IIS/Marko_Kovacevic_IIS/Marko_Kovacevic_IIS/Startup.cs
@@ -0,0 +1,69 @@
+﻿using Marko_Kovacevic_iis.Model;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Marko_Kovacevic_iis.Model.PredavacArray;
+
+namespace Marko_Kovacevic_iis
+{
+    internal class Startup
+    {
+        internal static PredavacArray PredavacArray;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+
+            List<Predavac> predavac = new List<Predavac>();
+            PredavacArray = new PredavacArray(predavac);
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddCors(Options => {
+                Options.AddPolicy("Cors", builder =>
+                {
+                    builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+                });
+            });
+            services.AddControllers().AddXmlDataContractSerializerFormatters();
+
+            services.AddMvc();
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "RESTApiIIS v1"));
+            }
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
