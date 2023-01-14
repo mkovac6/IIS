7364be60b4d49b1e2b6ebb5ecb6f484b86b76d40
Marko Kovačević <mkovac6@racunarstvo.hr>
2023-01-14 14:20:47 +0100

    SOAP sučelje koje uključuje servis koji prima termin po kojem će se pretraživati entitet, a na „backendu“ će generirati XML datoteku koje sadrži informacije o svim entitetima, nakon čega će se ta generirana datoteka pretraživati korištenjem XPath izraza i vratiti rezultate

---
+++ IIS/Marko_Kovacevic_SOAP/SOAPIIS/SOAPIIS/Predavac.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Web;
+
+namespace SOAPIIS
+{
+    [DataContract(Namespace = "http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model")]
+    public class PredavacArray
+    {
+        [DataMember(Order = 0)]
+        public List<Predavac> PredavacList { get; set; }
+
+        public PredavacArray()
+        {
+
+        }
+
+        public PredavacArray(List<Predavac> predavacList)
+        {
+            PredavacList = predavacList;
+        }
+
+        [DataContract(Namespace = "http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model")]
+        public class Predavac
+        {
+            public Predavac(string id, string type, string name, double placa)
+            {
+                Id = id;
+                Type = type;
+                Name = name;
+                Placa = placa;
+            }
+            public Predavac()
+            {
+
+            }
+            [DataMember(Order = 0)]
+            public string Id { get; set; }
+            [DataMember(Order = 1)]
+            public string Type { get; set; }
+            [DataMember(Order = 2)]
+            public string Name { get; set; }
+            [DataMember(Order = 3)]
+            public double Placa { get; set; }
+        }
+    }
+}
---
+++ IIS/Marko_Kovacevic_SOAP/SOAPIIS/SOAPIIS/WebService.asmx.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using static SOAPIIS.PredavacArray;
+
+namespace SOAPIIS
+{
+    /// <summary>
+    /// Summary description for WebService
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WebService : System.Web.Services.WebService
+    {
+        PredavacArray predArray = new PredavacArray();
+        List<Predavac> predavacList = new List<Predavac>();
+        Predavac pred1 = new Predavac("1", "Visi predavac", "Marko", 8000.145);
+        Predavac pred2 = new Predavac("2", "Nositelj kolegija", "Ana", 10000.177);
+        Predavac pred3 = new Predavac("3", "Predavac", "Marta", 14784.4154);
+        [WebMethod]
+
+        public List<Predavac> EntitySearch(string word)
+        {
+            predavacList.Add(pred1);
+            predavacList.Add(pred2);
+            predavacList.Add(pred3);
+            predArray.PredavacList = predavacList;
+
+            DataContractSerializer dcs = new DataContractSerializer(typeof(PredavacArray));
+            string path = @"C:\Users\marko\Desktop\School\IIS\IIS\Marko_Kovacevic_IIS\Marko_Kovacevic_IIS\Predavac.xml";
+            using (FileStream fs = new FileStream(path, FileMode.Create))
+            {
+                dcs.WriteObject(fs, predArray);
+                fs.Close();
+            }
+
+            XmlDocument xmldoc = new XmlDocument();
+            xmldoc.Load(path);
+            XmlNode root = xmldoc.DocumentElement;
+
+            XmlNamespaceManager manager = new XmlNamespaceManager(xmldoc.NameTable);
+            manager.AddNamespace("ns", "http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model");
+
+            XmlNodeList list = root.SelectNodes("/ns:PredavacArray/ns:PredavacList/ns:Predavac[ns:Type='" + word + "']", manager);
+
+            List<Predavac> result = new List<Predavac>();
+
+            for (int i = 0; i < list.Count; i++)
+            {
+                var currentNodeXml = "<Predavac manager=\"http://schemas.datacontract.org/2004/07/Marko_Kovacevic_iis1.Model\">" + list[i].InnerXml + "</Predavac>";
+                Stream ms = new MemoryStream(Encoding.UTF8.GetBytes(currentNodeXml));
+
+                DataContractSerializer ds = new DataContractSerializer(typeof(Predavac));
+                result.Add((Predavac)ds.ReadObject(ms));
+            }
+
+            return result;
+        }
+    }
+}
