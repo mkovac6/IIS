eeea7587666c1a8cc6131f05327755cc072f85e9
Marko Kovačević <mkovac6@racunarstvo.hr>
2023-01-29 13:46:24 +0100

    Pronaći proizvoljan javni REST API i integrirati se s njime korištenjem sigurnosnih ključeva ili implementirati vlastiti REST API koji će implementirati sigurnosne aspekte korištenjem JWT tokena.

---
+++ IIS/Marko_Kovacevi_API/Marko_Kovacevi_API/Program.cs
@@ -0,0 +1,32 @@
+﻿using ErgastApi.Client;
+using ErgastApi.Ids;
+using ErgastApi.Requests;
+using ErgastApi.Responses;
+
+var client = new ErgastClient();
+
+var request = new RaceResultsRequest
+{
+    Season = Seasons.Current,
+    Round = Rounds.Last,
+    DriverId = Drivers.MaxVerstappen,
+
+    Limit = 25,
+    Offset = 0,
+};
+
+
+RaceResultsResponse response = await client.GetResponseAsync(request);
+
+var race = response.Races.First();
+
+Console.WriteLine("Showing up to date data for driver: " + Drivers.MaxVerstappen);
+Console.WriteLine("Race round: " + race.Round);
+Console.WriteLine("Name of the race: " + race.RaceName);
+Console.WriteLine("Name of the circuit: " + race.Circuit.CircuitName);
+
+var driver = race.Results[0];
+
+Console.WriteLine("Driver code: " + driver.Driver.Code);
+Console.WriteLine("Fastest lap number: " + driver.FastestLap.LapNumber);
+Console.WriteLine("Driver position at the end of the race: " + driver.Position);
