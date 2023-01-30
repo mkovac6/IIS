3009edda85ee6d8cb6d66785ad039bfa17b7880d
Marko Kovačević <mkovac6@racunarstvo.hr>
2023-01-30 13:39:57 +0100

    BONUS ZADATAK: Studenti imaju mogućnost da do maksimalno 30% bodova iz jednog ishoda učenja ostvare rješavanjem dodatne funkcionalnosti koja uključuje korištenje RabbitMQ biblioteke.

---
+++ IIS/Marko_Kovacevic_RabbitMQ/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "IIS",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($"Nova poruka primljena: {message}");
+};
+
+channel.BasicConsume(queue: "IIS", autoAck: true, consumer: consumer);
+
+Console.WriteLine("Čitam....");
+
+Console.ReadKey();
---
+++ IIS/Marko_Kovacevic_RabbitMQ/Producer/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory() { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "IIS",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "Ovo je poruka pošaljitelja! Pozdrav za IIS!";
+
+var body = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "IIS", null, body);
+
+Console.WriteLine($"Poruka poslana: {message}");
